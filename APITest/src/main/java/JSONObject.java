52891f34374440151aa5ce691e7302495fdef634
TuLuongg <107625316+TuLuongg@users.noreply.github.com>
2022-07-05 20:29:27 +0700

    Add files via upload

---
+++ APITest/src/main/java/JSONObject.java
@@ -0,0 +1,20 @@
+package org.json;
+
+public class JSONObject {
+
+	public void put(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getString(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object get(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
