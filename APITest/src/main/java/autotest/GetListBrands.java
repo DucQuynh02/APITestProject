30cebffa2238b8a10923eb40fc71a1e834bf40a7
TuLuongg <107625316+TuLuongg@users.noreply.github.com>
2022-07-08 08:53:05 +0700

    Update GetListBrands.java

--- APITest/src/main/java/autotest/GetListBrands.java
+++ APITest/src/main/java/autotest/GetListBrands.java
@@ -9,13 +9,18 @@ import org.testng.Assert;
 
 import io.restassured.response.Response;
 
-public class GetListBrands extends APINeedTesting {
-	public String creRequest(String... request) {
+public class GetListBrands {
+
+	private int codeResponse;
+	private String messageResponse;
+	private String dataResponse;
+
+	public String creRequest(String... request) {		
 		return null;
 	}
-	
+
 	public void callAPI() {
-		baseURI = Constant.BaseURL;
+		baseURI = BaseURL.BASEURI;
 		Response response = 
 				given()
 				.when()
@@ -25,23 +30,22 @@ public class GetListBrands extends APINeedTesting {
 		this.codeResponse = Integer.parseInt(rep.get("code").toString());
 		this.messageResponse = rep.get("message").toString();
 		this.dataResponse = rep.get("data").toString();
-	
 	}
 	
 	void test1() {
 		System.out.println("Test 1 in GetListBrands API: The code should be 1000 and message is OK:");
 		
 		//Unit 1
-		try {
-			this.callAPI();
-			Assert.assertEquals(this.codeResponse, 1000);
-			Assert.assertEquals(this.messageResponse, "OK");
-	        System.out.println("Unit 1: Passed");
-	        System.out.println(this.dataResponse);
-		} catch (AssertionError e) {
-			System.out.println("Unit 1: Failed");
-		}
+		//Unit 1
+				try {
+					this.callAPI();
+					Assert.assertEquals(this.codeResponse, 1000);
+					Assert.assertEquals(this.messageResponse, "OK");
+			        System.out.println("Unit 1: Passed");
+			        System.out.println(this.dataResponse);
+				} catch (AssertionError e) {
+					System.out.println("Unit 1: Failed");
+				}
 
-	 }
-		
+ }
 }
