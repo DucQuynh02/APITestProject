e882a640984c52611ca98445c3154d4adf89d597
danghuy249 <108748410+danghuy249@users.noreply.github.com>
2022-07-09 23:53:38 +0700

    Update ChooseTest.java

--- APITest/src/main/java/testmain/ChooseTest.java
+++ APITest/src/main/java/testmain/ChooseTest.java
@@ -460,9 +460,6 @@ public class ChooseTest {
 			System.out.println("1: Get Detail Auction test 1: Dang dien ra ");
 			System.out.println("2: Get Detail Auction test 2: Sap dien ra");
 			System.out.println("3: Get Detail Auction test 3: Da ket thuc");
-			System.out.println("4: Get Detail Auction test 4: Dang cho phe duyet");
-			System.out.println("5: Get Detail Auction test 5: Da bi tu choi");
-			System.out.println("6: Get Detail Auction test 6: Da ban");
 			luachonTest = sc.nextInt();
 			sc.nextLine();
 			GetDetailAuction da = new GetDetailAuction();
@@ -482,15 +479,6 @@ public class ChooseTest {
 			}
 			else if(luachonTest==3) {
 				da.DA3();
-			}
-			else if(luachonTest==4) {
-				da.DA4();
-			}
-			else if(luachonTest==5) {
-				da.DA5();
-			}
-			else if(luachonTest==6) {
-				da.DA6();
 			}
 		}
 		else if(luachonAPI==10) {
