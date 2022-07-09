122a34eef40b1531e5c69aacfb0d0dfebaed08e6
TuLuongg <107625316+TuLuongg@users.noreply.github.com>
2022-07-09 23:47:20 +0700

    Update GetSlider.java

--- APITest/src/main/java/autotest/GetSlider.java
+++ APITest/src/main/java/autotest/GetSlider.java
@@ -55,6 +55,7 @@ public class GetSlider {
 	
 	public void test2() {
 		System.out.println("Test 2 of GetSlider API: chưa đăng nhập");
+		this.getAccessToken("","");
 		this.callAPI(access_token);
 		System.out.println("Code: "+this.codeResponse+"    Message: "+this.messageResponse+"    Data:"+this.dataResponse);
 		if(this.codeResponse.equals("1000") && !this.messageResponse.equals(""))
